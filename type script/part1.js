76244b4fe767061dca16ca34d440c05abf526b36
sekhar1245 <sekhar1245@gmail.com>
2020-03-05 07:51:57 +0000

    intial commit for Notes

---
+++ type script/part1.js
@@ -0,0 +1,8 @@
+var uname = "Scott";
+var age = 25;
+var isValidUser = true;
+console.log("User Name   :  " + uname);
+console.log("User Age   :  " + age);
+console.log("Is Valid User Name   :  " + isValidUser);
+var notyp = "not defining any type";
+console.log("no type   :  " + notyp);
---
+++ type script/part1.ts
@@ -0,0 +1,11 @@
+var uname:  string  =  "Scott";
+var age:  number  =  25;
+var isValidUser:  boolean  = true;
+
+console.log( "User Name   :  " + uname );
+console.log( "User Age   :  " + age );
+console.log( "Is Valid User Name   :  " + isValidUser );
+
+
+var notype= "not defining any type";
+console.log( "no type   :  " + notype );
